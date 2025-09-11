280ef776b815d0e4aaee3f9576f75dcddd0e21dc
gdy <lj1788@gmail.com>
2025-09-11 15:32:23 +0800

    fix

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -2,8 +2,10 @@ import 'regenerator-runtime/runtime'
 import 'core-js/stable'
 
 import React from 'react'
-import { render } from 'react-dom'
+import { createRoot } from 'react-dom/client'
 
 import { App } from './app'
 
-render(<App />, document.getElementById('app')!)
+const container = document.getElementById('app')!
+const root = createRoot(container)
+root.render(<App />)
